d79984f6d5582882f76be1e64705e94b32a38337
Manraaj Nijjar <ManraajNijjar@gmail.com>
2017-10-11 17:57:37 -0400

    Feat: Added testing methods

--- server/server.js
+++ server/server.js
@@ -13,8 +13,7 @@ app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
   });
-
-  todo.save().then(() => {
+  todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
     res.status(400).send(e);
@@ -24,3 +23,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {app};
